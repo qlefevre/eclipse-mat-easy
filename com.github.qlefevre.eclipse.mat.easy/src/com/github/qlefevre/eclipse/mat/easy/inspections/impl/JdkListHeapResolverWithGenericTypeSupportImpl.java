f1fe6570b29b9981775b188b9d3d4676c6db9373
Quentin <qlefevre@gmail.com>
2018-08-23 13:11:59 +0200

    List JDK9 implementations

--- com.github.qlefevre.eclipse.mat.easy/src/com/github/qlefevre/eclipse/mat/easy/inspections/impl/JdkListHeapResolverWithGenericTypeSupportImpl.java
+++ com.github.qlefevre.eclipse.mat.easy/src/com/github/qlefevre/eclipse/mat/easy/inspections/impl/JdkListHeapResolverWithGenericTypeSupportImpl.java
@@ -11,6 +11,8 @@ package com.github.qlefevre.eclipse.mat.easy.inspections.impl;
 
 import static com.github.qlefevre.eclipse.mat.easy.inspections.CollectionImplementations.JAVA_UTIL_ARRAYLIST;
 import static com.github.qlefevre.eclipse.mat.easy.inspections.CollectionImplementations.JAVA_UTIL_ARRAYS_ARRAYLIST;
+import static com.github.qlefevre.eclipse.mat.easy.inspections.CollectionImplementations.JAVA_UTIL_IMMUTABLECOLLECTIONS_LIST1;
+import static com.github.qlefevre.eclipse.mat.easy.inspections.CollectionImplementations.JAVA_UTIL_IMMUTABLECOLLECTIONS_LIST2;
 import static com.github.qlefevre.eclipse.mat.easy.inspections.CollectionImplementations.JAVA_UTIL_IMMUTABLECOLLECTIONS_LISTN;
 
 import org.eclipse.mat.SnapshotException;
@@ -31,10 +33,28 @@ public class JdkListHeapResolverWithGenericTypeSupportImpl
 
 	@Override
 	public int getCollectionSize(IObject object) throws SnapshotException {
-		int size = super.getCollectionSize(object);
-		if (size == -1) {
-			IObjectArray array = (IObjectArray) object.resolveValue("a");
+		int size;
+		IObjectArray array;
+		String classname = object.getClazz().getName();
+		switch (classname) {
+		case JAVA_UTIL_ARRAYS_ARRAYLIST:
+			array = (IObjectArray) object.resolveValue("a");
 			size = array != null ? array.getLength() : -1;
+			break;
+		case JAVA_UTIL_IMMUTABLECOLLECTIONS_LIST1:
+			size = 1;
+			break;
+		case JAVA_UTIL_IMMUTABLECOLLECTIONS_LIST2:
+			size = 2;
+			break;
+		case JAVA_UTIL_IMMUTABLECOLLECTIONS_LISTN:
+			array = (IObjectArray) object.resolveValue("elements");
+			size = array != null ? array.getLength() : -1;
+			break;
+		case JAVA_UTIL_ARRAYLIST:
+		default:
+			Integer resolvedSize = (Integer) object.resolveValue("size");
+			size = resolvedSize != null ? resolvedSize : -1;
 		}
 		return size;
 	}
@@ -54,6 +74,12 @@ public class JdkListHeapResolverWithGenericTypeSupportImpl
 			break;
 		case JAVA_UTIL_ARRAYS_ARRAYLIST:
 			storageObjects = new Object[] { object.resolveValue("a") };
+			break;
+		case JAVA_UTIL_IMMUTABLECOLLECTIONS_LIST1:
+			storageObjects = new Object[] { object.resolveValue("e0") };
+			break;
+		case JAVA_UTIL_IMMUTABLECOLLECTIONS_LIST2:
+			storageObjects = new Object[] { object.resolveValue("e0"), object.resolveValue("e1") };
 			break;
 		case JAVA_UTIL_IMMUTABLECOLLECTIONS_LISTN:
 			storageObjects = new Object[] { object.resolveValue("elements") };
